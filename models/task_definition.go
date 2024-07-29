ee0644207981dde131c667abc6f00ca4b2fb75ec
Michel Wilhelm <michelwilhelm@gmail.com>
2024-07-29 09:33:52 -0300

    ♻️ (task_definition.go): refactor TaskDefinition struct for better readability and consistency ✨ (task_definition.go): add FlowDefinitionID field and logic to fetch FlowDefinition by reference ID

--- models/task_definition.go
+++ models/task_definition.go
@@ -9,35 +9,44 @@ import (
 )
 
 type TaskDefinition struct {
-	ID           int             `json:"id" gorm:"primaryKey"`
-	CreatedAt    time.Time       `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt    time.Time       `json:"updated_at" gorm:"autoUpdateTime"`
-    ReferenceID  string          `json:"reference_id"`
-	Name         string          `json:"name"`
-	Description  string          `json:"description,omitempty"`
-    FlowReferenceID string       `json:"flow_reference_id"`
-	ParentTaskID *int            `json:"parent_task_id"`
-	InputSchema  JSONB           `json:"input_schema" gorm:"type:jsonb"`
-	OutputSchema JSONB           `json:"output_schema" gorm:"type:jsonb"`
-    Input        JSONB           `json:"input" gorm:"-"`
-	Output       JSONB           `json:"output" gorm:"-"`
-    Version      int             `json:"version" default:"1"`
-    Metadata     JSONB           `json:"metadata" gorm:"type:jsonb"`
+	ID               int       `json:"id" gorm:"primaryKey"`
+	CreatedAt        time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt        time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	ReferenceID      string    `json:"reference_id"`
+	Name             string    `json:"name"`
+	Description      string    `json:"description,omitempty"`
+	FlowDefinitionID int       `json:"flow_definition_id"`
+	FlowReferenceID  string    `json:"flow_reference_id"`
+	ParentTaskID     *int      `json:"parent_task_id"`
+	InputSchema      JSONB     `json:"input_schema" gorm:"type:jsonb"`
+	OutputSchema     JSONB     `json:"output_schema" gorm:"type:jsonb"`
+	Input            JSONB     `json:"input" gorm:"-"`
+	Output           JSONB     `json:"output" gorm:"-"`
+	Version          int       `json:"version" default:"1"`
+	Metadata         JSONB     `json:"metadata" gorm:"type:jsonb"`
 }
 
 // BeforeCreate hook
 func (f *TaskDefinition) BeforeCreate(tx *gorm.DB) (err error) {
-    flowspellHost := os.Getenv("FLOWSPELL_HOST")
+	flowspellHost := os.Getenv("FLOWSPELL_HOST")
 	if f.ReferenceID == "" {
 		f.ReferenceID = uuid.New().String()
 	}
 
-    // Default Metadata
+	// Get FlowDefinition by reference_id the last version
+	flowDefinition, err := (&FlowDefinition{}).GetFlowDefinitionByReferenceID(tx, f.FlowReferenceID)
+	if err != nil {
+		return
+	}
+
+	f.FlowDefinitionID = flowDefinition.ID
+
+	// Default Metadata
 	if f.Metadata == nil {
 		f.Metadata = make(map[string]interface{})
 	}
 
-    // Verify if the f.Input schema is valid
+	// Verify if the f.Input schema is valid
 	convertedInput, err := ConvertJSONBToSimplifiedSchema(f.Input)
 	if err != nil {
 		return err
@@ -49,14 +58,14 @@ func (f *TaskDefinition) BeforeCreate(tx *gorm.DB) (err error) {
 		Type:        "input",
 	}
 
-    completeInputSchema, err := CompleteSchema(convertedInput, schemaDataInput)
+	completeInputSchema, err := CompleteSchema(convertedInput, schemaDataInput)
 	if err != nil {
 		return err
 	}
 
 	f.InputSchema = completeInputSchema
 
-    // Verify if the f.Output schema is valid
+	// Verify if the f.Output schema is valid
 	convertedOutput, err := ConvertJSONBToSimplifiedSchema(f.Output)
 	if err != nil {
 		return err
@@ -75,7 +84,7 @@ func (f *TaskDefinition) BeforeCreate(tx *gorm.DB) (err error) {
 
 	f.OutputSchema = completeOutputSchema
 
-    f.CreatedAt = time.Now()
+	f.CreatedAt = time.Now()
 	f.UpdatedAt = time.Now()
 	if f.Version == 0 {
 		f.Version = 1
@@ -83,5 +92,5 @@ func (f *TaskDefinition) BeforeCreate(tx *gorm.DB) (err error) {
 
 	f.Version++
 
-    return
+	return
 }

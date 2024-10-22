b07de6431f149cdd29198638d352e7c0dbb5af22
Michel Wilhelm <michelwilhelm@gmail.com>
2024-10-21 23:49:37 -0300

    Fixing start script (#6)

--- integration/commands/start.py
+++ integration/commands/start.py
@@ -10,5 +10,7 @@ flow_instance_data = {
     },
 }
 
+flow_definition_ref_id = "f29c8333-2e2e-4690-90e1-aa86fdaa78ef"
+
 response_flow_instance = client().post(f"/flows/instances/{flow_definition_ref_id}/start", json=flow_instance_data)
 assert response_flow_instance.status_code == 201
